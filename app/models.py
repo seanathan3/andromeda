37f327717bca75b5b81b4ba895bdcec2974454db
seanathan3 <sean.abbas33@gmail.com>
2024-12-28 16:27:31 -0600

    set up viewport

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Player(models.Model):
+    username = models.CharField(max_length=30)
+    health = models.IntegerField()
+    energy = models.IntegerField()
+
+    def __str__(self):
+        return self.username
+
+class Card(models.Model):
+    name = models.CharField(max_length=30, default='name')
+    health = models.IntegerField()
+    attack = models.IntegerField()
+
+    def __str__(self):
+        return self.name
